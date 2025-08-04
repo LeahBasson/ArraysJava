2d72956bb83adc7b652124c21ebdb8d735fdab99
LeahBasson <leahbasson54@gmail.com>
2025-08-04 16:58:21 +0200

    :fire:

---
+++ ShopApplication/src/ShopApplication.java
@@ -0,0 +1,63 @@
+
+import java.util.Scanner;
+
+public class ShopApplication {
+
+    public static void main(String[] args) {
+        // Create an array of item names and prices.
+        // Display every item name and its corresponding price.
+        // Print the average price of all the items.
+        // Find and display the price of a specific item.
+
+        String[] item_names = {"Bread", "Milk", "Butter", "Chips", "Honey", "Soap", "Towel", "Carrots", "Beans", "Samp", "Dress", "Pants", "Shoes", "Socks"};
+        double[] item_prices = {15.50, 12.00, 56.00, 20.00, 45.00, 7.00, 75.00, 8.00, 5.00, 12.00, 200.00, 150.00, 400.00, 25.00};
+
+        System.out.println("Items in stock with prices");
+        System.out.println("--------------------------");
+
+        double sum = 0, avg;
+
+        for (int i = 0; i < item_names.length; i++) {
+            // Display all items
+            System.out.println(item_names[i] + " - R" + item_prices[i]);
+
+            // sum calculation
+            sum += item_prices[i];
+
+        }
+
+        avg = sum / item_prices.length;
+        System.out.printf("The average price of all the items is R" + "%.1f%n", avg);
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("\nItem Look-up");
+        System.out.println("What item are you looking for? ");
+        String item_lookup = scn.next();
+        
+        boolean found = false;
+        
+        for(int i = 0; i < item_names.length; i++){
+            if(item_names[i].equalsIgnoreCase(item_lookup)){
+                System.out.println("The price of item " + item_names[i] + " is R" + item_prices[i]);
+                found = true;
+                break;
+            }
+        }
+        
+        if (!found){
+            System.out.println("Item " + item_lookup + " is not stocked in the shop.");
+        }
+
+//        for (int i = 0; i < item_names.length; i++) {
+//
+//            if (item_lookup.equals(item_names[i])) {
+//                System.out.println("The price of item " + item_lookup + " is R" + item_prices[i]);
+//            }
+//
+//        }
+        
+
+    } // end of main
+
+} // end of class
