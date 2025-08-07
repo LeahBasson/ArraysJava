d322851f4a423ba4b7d3ef9600d0321804216757
LeahBasson <leahbasson54@gmail.com>
2025-08-07 17:45:29 +0200

    :fire:

--- Tutorial34/src/Tutorial34.java
+++ Tutorial34/src/Tutorial34.java
@@ -1,12 +1,15 @@
-package tut3.pkg4;
 
-public class Tut34 {
+import java.util.Scanner;
+
+public class Tutorial34 {
 
     public static void main(String[] args) {
         // Two parallel arrays are to be used to store the surnames of fundraisers.
         // Respective amounts of funds.
         // display the surname and amount of the fundraiser that raised the most
         // display the surname and amount of the fundraiser that raised the least
+        // Input a surname from the user and display the amount they raised, if found in array.
+        Scanner kbd = new Scanner(System.in);
 
         String[] surnames = {"Malan", "Jones", "West", "Nadile", "King", "Bester", "Peters", "Adams"};
         double[] amounts = {50000, 80000, 45000, 42000, 61000, 30000, 49000, 25000};
@@ -21,23 +24,40 @@ public class Tut34 {
         int minIndex = 0;
 
         for (int i = 1; i < amounts.length; i++) {
-            
+
             if (amounts[maxIndex] < amounts[i]) {
                 maxIndex = i; // index you are comparing to.  
                 //System.out.println(surnames[maxIndex]+" maxIndex = "+maxIndex+" = "+ amounts[maxIndex]);
             }
-            
+
             //It goes through the loop and everytime the amount gets lower it changes the index.
             if (amounts[minIndex] > amounts[i]) {
                 minIndex = i; //index you are comparing to.  
                 //System.out.println(surnames[minIndex]+ " minIndex = "+minIndex+" = "+ amounts[minIndex]);
             }
         }
-        System.out.println(surnames[maxIndex] + " raised the MOST funds: ");
-        
-         
+        System.out.println("\n" + surnames[maxIndex] + " raised the MOST funds: R" + amounts[maxIndex]);
+        System.out.println(surnames[minIndex] + " raised the LEAST funds: R" + amounts[minIndex]);
 
-    
-    } //the end of main
+        System.out.print("\nPlease enter a surname: ");
+        String name = kbd.next();
 
-} // the end of class
+        boolean found = false;
+        int indexFound = 0;
+
+        while ((indexFound < amounts.length) && (!found)) {
+            if (surnames[indexFound].equals(name)) {
+                found = true;
+            } else {
+                indexFound++;
+            }
+        }
+        if (found){
+            System.out.println(name + " found - they raised R" + amounts[indexFound]);
+        } else{
+            System.out.println("Name not found."); 
+        }
+
+    }// end of main
+
+}// end of class
