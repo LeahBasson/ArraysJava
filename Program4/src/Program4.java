8e622853f2d4397816daff8bc7247bf598dc7354
LeahBasson <leahbasson54@gmail.com>
2025-08-10 19:12:48 +0200

    :lipstick:

--- Program4/src/Program4.java
+++ Program4/src/Program4.java
@@ -1,7 +1,6 @@
 
 import java.util.Scanner;
 
-
 public class Program4 {
 
     public static void main(String[] args) {
@@ -11,6 +10,7 @@ public class Program4 {
         // must allow the user to enter an English phrase, and then display its translation (if there is one).
         // and increase that phrase's counter by 1. This must continue until the user types in "X" as the English phrase.
         // must display: most often searched-for phrase, its translation, the number of times that it was searched for (or looked up), for its translation.
+        Scanner kbd = new Scanner(System.in);
 
         String[] engPhr = {"Good morning", "My name is", "How are you",
             "Good bye", "My number is", "I would like some", "Coffee",
@@ -21,38 +21,63 @@ public class Program4 {
             "Moje cislo je", "Prosim si", "Kava", "Hranloy", "Pivo", "Zmrzlina",
             "Caj", "Na zdravie", "Mozem mat ucet", "Prosim", "Dakujem"};
 
-        int[] nrLookUps = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        
+        int[] nrLookUps = new int[engPhr.length];
+
         System.out.println("Translating list of phrases into Slovak");
         System.out.println("---------------------------------------");
-        System.out.println("English Phrases: ");
-        
-        for (int i = 0; i < engPhr.length; i++){
-            System.out.println(engPhr[i]);
+
+        // prints of list of phrases
+        for (int i = 0; i < engPhr.length; i++) {
+            System.out.println(engPhr[i] + " - " + sloPhr[i]);
         }
-        
-        Scanner kbd = new Scanner(System.in);
-        
-        System.out.print("\nEnter an english phrase: ");
-        String userSearch = kbd.next();
-        
-        boolean found = false;
-        int indexFound = 0;
-        
-        while(!userSearch.equals("x")){ 
-            while((indexFound < engPhr.length) && (!found)){
-                if (engPhr[indexFound].equals(userSearch)){
+
+        System.out.print("\n");
+        // user input
+        System.out.print("Enter an english phrase (or x to stop): ");
+        String userSearch = kbd.nextLine().trim();
+
+        // loop for user input and search.
+        while (!userSearch.equalsIgnoreCase("x")) {
+            // has to be inside here so to make it false again when new user input is entered.
+            boolean found = false;
+            int index = 0;
+
+            // checks if user input is a match
+            while ((index < engPhr.length) && (!found)) {
+                if (engPhr[index].equalsIgnoreCase(userSearch)) {
+                    nrLookUps[index]++;
                     found = true;
                 } else {
-                    indexFound ++;
+                    index++;
                 }
             }
-            
-            System.out.print("Enter an english phrase: ");
-            userSearch = kbd.next();
+
+            // prints out message for the results from the check.
+            if (found) {
+                System.out.println(
+                        "Translation: " + userSearch + " is " + sloPhr[index]);
+            } else {
+                System.out.println("Phrase not found.");
+            }
+
+            System.out.print("Enter an english phrase (or x to stop): ");
+            userSearch = kbd.nextLine().trim();
+        }
+
+        // finds the most searched phrase.
+        int maxIndex = 0;
+
+        for (int i = 1; i < nrLookUps.length; i++) {
+            if (nrLookUps[maxIndex] < nrLookUps[i]) {
+                maxIndex = i; // index you are comparing to.  
+            }
         }
         
-       
+        // prints message for most searched, translation & number of times.
+        System.out.println(
+                "The most often searched for phrase is " + engPhr[maxIndex] + " it's translation is " + sloPhr[maxIndex]);
+        System.out.println(
+                "The number of times it was searched: " + nrLookUps[maxIndex]);
 
     }// end of main
 
