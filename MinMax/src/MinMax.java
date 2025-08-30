ea3c7dc5ee1e30dad0cf292cf1a67baa51896968
LeahBasson <leahbasson54@gmail.com>
2025-08-30 21:56:23 +0200

    :rocket:

--- MinMax/src/MinMax.java
+++ MinMax/src/MinMax.java
@@ -1,37 +1,9 @@
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class MinMax {
 
     public static void main(String[] args) {
         // array of numbers
         int[] numbers = {5, 6, 1, 3, 9, 4};
-        String[] names = new String[3];
-
-        int add = 0;
-        int cnt = 1;
-        
-        Scanner kbd = new Scanner(System.in);
-
-        System.out.print("Enter a name (type stop to finish): ");
-        String name = kbd.next();
-
-        while (!"stop".equals(name) && cnt < names.length) {
-               
-            names[add] = name;
-            add++;
-            cnt++;
-            
-            System.out.print("Enter a name (type stop to finish): ");
-            name = kbd.next();
-        }
-        
-//        for(int i = 0; i < names.length; i++){
-//            System.out.println("Name: " + names[i]);
-//        }
-        
-        System.out.println("names: " + Arrays.toString(names));
 
         int minNum = 0;
         int maxNum = 0;
---
+++ numbersEnter/src/numbersenter/NumbersEnter.java
@@ -0,0 +1,30 @@
+
+package numbersenter;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class NumbersEnter {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5]; // array of size 5
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter a number: ");
+            numbers[i] = sc.nextInt(); // read user input
+        }
+        System.out.println("numbers: " + Arrays.toString(numbers));
+  
+        String[] names = new String[5];
+        
+        for(int i = 0; i < names.length; i++){
+            System.out.print("Enter a name: ");
+            names[i] = sc.next();
+        }
+        System.out.println("names: " + Arrays.toString(names));
+    } // end of main
+    
+} // end of class
